ee21efdea2edc928025b42d2d87cbb072f9e00e2
Luong Hoai Canh <canhluong@neo-lab.vn>
2017-01-04 18:52:23 +0700

    update

--- travels/source_code/frontend/views/common/navbar.php
+++ travels/source_code/frontend/views/common/navbar.php
@@ -8,11 +8,23 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="."><i class="glyphicon glyphicon-phone"></i> PHP 05 (test)</a>
+			<a class="navbar-brand" href="."><i class="glyphicon glyphicon-plane"></i> PHP 05 (test)</a>
 		</div>
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="index.php?controller=home"><i class="glyphicon glyphicon glyphicon-home"></i> Home</a>
+				</li>
+				<li>
+					<a href="index.php?controller=tour"><i class="glyphicon glyphicon glyphicon-lamp"></i> Tour</a>
+				</li>
+				<li>
+					<a href="index.php?controller=product"><i class="glyphicon glyphicon glyphicon-gift"></i> Shop</a>
+				</li>
+				<li>
+					<a href="index.php?controller=contact"><i class="glyphicon glyphicon glyphicon-copyright-mark"></i> Contact</a>
+				</li>
 				<li>
 					<a href="index.php?controller=cart"><i class="glyphicon glyphicon-shopping-cart"></i> Giỏ hàng : <?php echo cart_number(); ?> sản phẩm</a>
 				</li>
